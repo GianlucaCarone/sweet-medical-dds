967fb19180c42c756a54e43c7ac0e5f08f43277a
Andino Franco <fandino@frba.utn.edu.ar>
2026-04-20 20:57:48 -0300

    Se cambia el mensaje de las notificaciones

--- packages/backend/domain/factoryNotificacion.js
+++ packages/backend/domain/factoryNotificacion.js
@@ -2,17 +2,38 @@ import { Notificacion } from "../domain/notificacion";
 import { EstadoTurno } from "./turno";
 
 
+// TODO Investigar I18NN para manejar los mensajes de las notificaciones en diferentes idiomas.
 
 class FactoryNotificacion {
   crearSegunEstadoTurno(turno) {
     switch (turno.estado) {
       case EstadoTurno.RESERVADO:
-        // TODO consultar el tema de servicio (especialidad o practica)
-        return new Notificacion({ destinatario: turno.medico, remitente: turno.paciente, mensaje: "El turno fue reservado por el paciente " + turno.paciente.nombre + " para la especialidad " + turno.especialidad.nombre + " con la practica " + turno.practica.nombre + " en la sede " + turno.sede.nombre });
+        return new Notificacion({
+          destinatario: turno.medico,
+          remitente: turno.paciente,
+          mensaje: 
+            `El turno fue reservado por el paciente ${turno.paciente.nombre} 
+            ${turno.servicio instanceof Especialidad ? 'para la especialidad' : 'con la practica'} ${turno.servicio.nombre} 
+            en la sede ${turno.sede.nombre}`
+        });
       case EstadoTurno.CANCELADO:
-        return new Notificacion({ destinatario: turno.medico, remitente: turno.paciente, mensaje: "El turno fue cancelado por el paciente " + turno.paciente.nombre + " para la especialidad " + turno.especialidad.nombre + " con la practica " + turno.practica.nombre + " en la sede " + turno.sede.nombre });
+        return new Notificacion({
+          destinatario: turno.medico,
+          remitente: turno.paciente,
+          mensaje: 
+            `El turno fue cancelado por el paciente ${turno.paciente.nombre} 
+            ${turno.servicio instanceof Especialidad ? 'para la especialidad' : 'con la practica'} ${turno.servicio.nombre} 
+            en la sede ${turno.sede.nombre}`
+        });
       case EstadoTurno.CONFIRMADO:
-        return new Notificacion({ destinatario: turno.paciente, remitente: turno.medico, mensaje: "El turno fue confirmado por el medico " + turno.medico.nombre + " para la especialidad " + turno.especialidad.nombre + " con la practica " + turno.practica.nombre + " en la sede " + turno.sede.nombre });
+        return new Notificacion({ 
+          destinatario: turno.paciente, 
+          remitente: turno.medico, 
+          mensaje: 
+            `El turno fue confirmado por el medico ${turno.medico.nombre}
+            ${turno.servicio instanceof Especialidad ? 'para la especialidad' : 'con la practica'} ${turno.servicio.nombre} 
+            en la sede ${turno.sede.nombre}`
+        });
       default:
         throw new Error("Estado de turno desconocido");
     }
