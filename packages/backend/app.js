998b1cf0edef8734c10ad5af8042da6d8bb69f99
lDomio <ldomio@frba.utn.edu.ar>
2026-05-09 18:05:52 -0300

    agrego dependencias de eslint, jest y supertest + cambio arquitectura para iniciar app + cambio de rutas para consistencia con arquitectura

---
+++ packages/backend/app.js
@@ -0,0 +1,43 @@
+import express from "express"; // framework para crear el servidor y manejar las rutas
+import cors from "cors"; // middleware para permitir solicitudes desde diferentes orígenes (CORS)
+import { notFoundHandler } from "./middlewares/notFoundHandler.js";
+import { errorLogger } from "./middlewares/errorLogger.js";
+import { errorHandler } from "./middlewares/errorHandler.js";
+import { Server } from "./server.js";
+import routes from "./routes/router.js";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use(notFoundHandler);
+app.use(errorLogger);
+app.use(errorHandler);
+
+app.use(
+  cors({
+    origin: process.env.ALLOWED_ORIGINS
+      ? process.env.ALLOWED_ORIGINS.split(",").map((o) => o.trim())
+      : true,
+  }),
+);
+
+
+app.listen(process.env.SERVER_PORT, () => {
+  console.warn(`Backend escuchando en puerto ${process.env.SERVER_PORT}`);
+});
+
+const server = new Server(app);
+
+/* -------------------------------------------------------------------------- */
+/*                 Aca van los controllers y repos por feature                */
+/* -------------------------------------------------------------------------- */
+
+/* -------------------------------------------------------------------------- */
+/*                                    RUTAS                                   */
+/* -------------------------------------------------------------------------- */
+routes.forEach(ruta => server.addRoute(ruta));
+server.configurarRutas();
+
+export default server;
