4f9a9668d08e30bba59d9702456ef479ceb16c45
lDomio <ldomio@frba.utn.edu.ar>
2026-05-10 19:40:57 -0300

    fix: .js faltante en algunos imports y typo en sede + instanciación de recurso sede + instaniacion #controllers + conexion a db terminada

--- packages/backend/index.js
+++ packages/backend/index.js
@@ -1,7 +1,7 @@
 import "dotenv/config"; // tiene todas las configuracione como: SERVER_PORT, ALLOWED_ORIGINS, etc. que se encuentran en el archivo .env
 import dotenv from "dotenv";
-import { ClienteMongoDb } from "./config/database";
-import server from "./app";
+import { ClienteMongoDb } from "./config/database.js";
+import server from "./app.js";
 
 dotenv.config();
 const port = process.env.SERVER_PORT;
