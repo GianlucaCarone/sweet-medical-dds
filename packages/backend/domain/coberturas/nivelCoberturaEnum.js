ba2910311dea56783869c7b83aeb0511ed93b5d4
matuaponte <maponte@frba.utn.edu.ar>
2026-05-12 15:34:20 -0300

    conexion y refactor para el repository con mongo, creadcion de models y schemas (#12)

--- packages/backend/domain/coberturas/nivelCoberturaEnum.js
+++ packages/backend/domain/coberturas/nivelCoberturaEnum.js
@@ -1,5 +1,6 @@
 const NivelCobertura = Object.freeze({
-  TOTAL:  "TOTAL",
-  PARCIAL:  "PARCIAL",
-  NO_CUBIERTA:   "NO_CUBIERTA",
+  TOTAL: "TOTAL",
+  PARCIAL: "PARCIAL",
+  NO_CUBIERTA: "NO_CUBIERTA",
 });
+
---
+++ packages/backend/schemas/dataBase/turnoSchemaDB.js
@@ -0,0 +1,60 @@
+import { mongoose } from 'mongoose';
+import { Turno } from '../../domain/turnos/turno.js';
+import { historialEstadoTurnoSchema } from './historialEstadoTurnoSchema.js';
+
+
+
+const turnoSchema = new mongoose.Schema({
+    _id: {
+        type: String,
+        required: true
+    },
+    fechaHora: {
+        type: Date,
+        required: true,
+    },
+    estado: {
+        type: String,
+        required: true,
+    },
+    historialEstado: [historialEstadoTurnoSchema],
+    medico: {
+        type: String, //referenciamos el uuid del medico 
+        ref: 'Medico',
+        required: true,
+    },
+    paciente: {
+        type: String,  //referenciamos el uuid del paciente 
+        ref: 'Paciente',
+        required: false,
+    },
+    practica: {
+        type: String, // referenciamos el uuid de la practica si eligió una practica
+        ref: 'Practica',
+        required: false,
+    },
+    especialidad: {
+        type: String, // referenciamos el uuid de la especialidad si eligió una especialidad
+        ref: 'Especialidad',
+        required: false,
+    },
+    sede: {
+        type: String, //referenciamos el uuid de la sede 
+        ref: 'Sede',
+        required: false,
+    },
+    costo: {
+        type: Number,
+        required: false,
+    },/* ver si lo usamos o no
+    eliminado: {
+        type: Boolean,
+        required: true,
+        default: false,
+    },*/
+});
+
+
+turnoSchema.loadClass(Turno);
+
+export const TurnoModel = mongoose.model('Turno', turnoSchema)
