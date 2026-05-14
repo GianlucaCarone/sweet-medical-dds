3c1df21904c2bfb9cb5bf4f214e9941b82618c14
lDomio <ldomio@frba.utn.edu.ar>
2026-05-14 15:04:13 -0300

    agrego enruteo correcto

--- packages/backend/routes/router.js
+++ packages/backend/routes/router.js
@@ -1,9 +1,10 @@
 import sedeRoutes from "./sedeRoutes.js";
 import healthRoutes from "./healthRoutes.js";
+import turnoRoutes from "./turnoRoutes.js";
 
 const routes = [
   { path: "/sede", handler: sedeRoutes },
   { path: "/health", handler: healthRoutes },
-  { path: "/turno", handler: healthRoutes }
+  { path: "/turno", handler: turnoRoutes }
 ];
 export default routes;
