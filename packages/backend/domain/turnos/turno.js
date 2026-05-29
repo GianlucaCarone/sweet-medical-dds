fd5e950d964c6aaae589e51f3b0d672c51854b24
matuaponte <maponte@frba.utn.edu.ar>
2026-05-29 15:40:06 -0300

    Refactor de mapper turno

--- packages/backend/domain/turnos/turno.js
+++ packages/backend/domain/turnos/turno.js
@@ -1,9 +1,6 @@
-import { Medico } from "../medico.js";
 import { ErrorDatosObligatorios } from "../errores.js";
 import { EstadoTurnoEnum } from "./estadoTurnoEnum.js";
 import { CambioEstadoTurno } from "./cambioEstadoTurno.js";
-import { Practica } from "../servicios/practica.js";
-import { Especialidad } from "../servicios/especialidad.js";
 import { Usuario } from "../usuario.js";
 export class Turno {
     id;
@@ -17,7 +14,7 @@ export class Turno {
     historialEstado;
     costo;
 
-    constructor({ medico, fechaHora, sede, servicio }) {
+    constructor({ medico, fechaHora, sede, servicio,costo = 0 }) {
         if (!medico || !sede || !fechaHora) {
             throw new ErrorDatosObligatorios();
         }
@@ -26,7 +23,7 @@ export class Turno {
         this.fechaHora = fechaHora;
         this.sede = sede;
         this.servicio = servicio;
-
+        this.costo = costo;
         this.estado = EstadoTurnoEnum.DISPONIBLE;
         this.historialEstado = [];
     }
