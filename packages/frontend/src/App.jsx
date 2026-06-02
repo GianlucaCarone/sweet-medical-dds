31fe7f0a4cdaeb4b3c22451ad343d435ea09e063
Andino Franco <fandino@frba.utn.edu.ar>
2026-06-01 21:12:13 -0300

    De nabo no subi el App.js

---
+++ packages/frontend/src/App.jsx
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
+import Layout from "./features/layout/Layout.jsx";
+
+import "./App.css";
+
+function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:8000/hello")
+      .then((response) => response.json())
+      .then((data) => setMessage(data.message))
+      .catch((error) => console.error("Error cargando mensaje.", error));
+  }, []);
+
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        {/* <Route index element={<Home />} /> */}
+      </Route>
+    </Routes>
+  );
+}
+
+export default App;
