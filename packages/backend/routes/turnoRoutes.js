a13573172aabbe0ca1753bca8e88a54b1f996d0b
Gianluca Carone <gcaronetonello@frba.utn.edu.ar>
2026-07-23 20:55:46 -0300

    cambio turnos

--- packages/backend/routes/turnoRoutes.js
+++ packages/backend/routes/turnoRoutes.js
@@ -105,7 +105,7 @@ export default function turnoRoutes(getController) {
          *       400:
          *         $ref: '#/components/responses/E400'
          */
-        .get(authMiddleware,(req, res, next) => turnoController.findAllPaginated(req, res, next))
+        .get((req, res, next) => turnoController.findAllPaginated(req, res, next))
         /**
          * @swagger
          * /turno:
